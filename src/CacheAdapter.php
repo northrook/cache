6f3af6617ed53b282687fe92db083345999bf635
Martin <hello@northrook.com>
2025-06-15 13:00:33 +0100

    mend

--- src/CacheAdapter.php
+++ src/CacheAdapter.php
@@ -4,14 +4,31 @@ declare(strict_types=1);
 
 namespace Cache;
 
-use Core\Contracts\Autowire\Logger;
 use Psr\Cache\CacheItemPoolInterface;
+use Psr\Log\{LoggerAwareInterface, LoggerInterface, NullLogger};
 
-abstract class CacheAdapter implements CacheItemPoolInterface
+abstract class CacheAdapter implements CacheItemPoolInterface, LoggerAwareInterface
 {
-    use Logger;
+    protected readonly LoggerInterface $logger;
 
     protected readonly string $name;
+
+    /**
+     * @param null|LoggerInterface $logger
+     * @param bool                 $assignNull
+     *
+     * @return void
+     */
+    final public function assignLogger(
+        ?LoggerInterface $logger,
+        bool             $assignNull = false,
+    ) : void {
+        if ( $logger === null && $assignNull === false ) {
+            return;
+        }
+
+        $this->logger = $logger ?? new NullLogger();
+    }
 
     final protected function setName( ?string $name = null ) : void
     {
